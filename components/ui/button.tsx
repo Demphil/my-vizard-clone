b081882501de28f5134c4e00490c2231d509b495
Demphil <92610592+Demphil@users.noreply.github.com>
2025-09-06 20:16:46 +0100

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,7 +3,10 @@ import { ButtonHTMLAttributes, FC } from 'react';
 
 const Button: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({ children, ...props }) => {
   return (
-    <button {...props} className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+    <button
+      {...props}
+      className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
+    >
       {children}
     </button>
   );
